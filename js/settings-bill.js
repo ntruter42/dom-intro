c83d91093cdb9b5b9b3c85ece8bd45913c85d5cf
Nicholas Truter <ntruter42@gmail.com>
2023-04-11 03:18:43 +0200

    Update index.html, Add settings-bill.js - created javascript for bill with settings widget

---
+++ js/settings-bill.js
@@ -0,0 +1,65 @@
+// SETTINGS ELEMENTS
+const settingsCallCost = document.querySelector("#settings-call-cost");
+const settingsSmsCost = document.querySelector("#settings-sms-cost");
+const settingsWarningLevel = document.querySelector("#settings-warning-level");
+const settingsCriticalLevel = document.querySelector("#settings-critical-level");
+
+// INPUT ELEMENTS
+const settingsButton = document.querySelector("#settings-button");
+const settingsReset = document.querySelector("#settings-reset");
+const settingsUpdate = document.querySelector("#settings-update");
+
+// OUTPUT ELEMENTS
+const settingsCallTotal = document.querySelector("#settings-call-total");
+const settingsSmsTotal = document.querySelector("#settings-sms-total");
+const settingsTotal = document.querySelector("#settings-total");
+
+let callSettingsTotal = 0;
+let smsSettingsTotal = 0;
+let callSettingsCost = 0;
+let smsSettingsCost = 0;
+let warningLevel = 0;
+let crtiticalLevel = 0;
+
+function settingsButtonClicked() {
+	const settingsChecked = document.querySelector("input[name='settings-bill-item']:checked").value.toLowerCase();
+
+	if (settingsChecked) {
+		if (settingsChecked === 'call') {
+			callSettingsTotal += callSettingsCost;
+		} else if (settingsChecked === 'sms') {
+			smsSettingsTotal += smsSettingsCost;
+		}
+		const total = callSettingsTotal + smsSettingsTotal;
+
+		settingsTotal.classList.remove("warning", "danger");
+		if (total > crtiticalLevel) {
+			settingsTotal.classList.add("danger");
+		} else if (total > warningLevel) {
+			settingsTotal.classList.add("warning");
+		}
+
+		settingsCallTotal.innerHTML = "R" + callSettingsTotal.toFixed(2);
+		settingsSmsTotal.innerHTML = "R" + smsSettingsTotal.toFixed(2);
+		settingsTotal.innerHTML = "R" + total.toFixed(2);
+	}
+}
+settingsButton.addEventListener('click', settingsButtonClicked);
+
+function resetSettingsTotals() {
+	callSettingsTotal = 0;
+	smsSettingsTotal = 0;
+	settingsCallTotal.innerHTML = "R0.00";
+	settingsSmsTotal.innerHTML = "R0.00";
+	settingsTotal.innerHTML = "R0.00";
+	settingsTotal.classList.remove("warning", "danger");
+}
+settingsReset.addEventListener('click', resetSettingsTotals);
+
+function updateSettingsValues() {
+	callSettingsCost = Number(settingsCallCost.value);
+	smsSettingsCost = Number(settingsSmsCost.value);
+	warningLevel = Number(settingsWarningLevel.value);
+	crtiticalLevel = Number(settingsCriticalLevel.value);
+}
+settingsUpdate.addEventListener('click', updateSettingsValues);
